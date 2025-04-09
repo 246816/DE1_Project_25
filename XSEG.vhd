b6994efe274de4511502d30cb139ed10ad154b09
W4nt0r <119078898+W4nt0r@users.noreply.github.com>
2025-04-09 12:09:56 +0200

    Minor changes

--- XSEG.vhd
+++ XSEG.vhd
@@ -1,55 +1,61 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/02/2025 01:15:51 PM
--- Design Name: 
--- Module Name: XSEG - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity XSEG is
-    Port ( clk : in STD_LOGIC;
-           seg : in STD_LOGIC;
-           CA : out STD_LOGIC;
-           CB : out STD_LOGIC;
-           CC : out STD_LOGIC;
-           CD : out STD_LOGIC;
-           CE : out STD_LOGIC;
-           CF : out STD_LOGIC;
-           CG : out STD_LOGIC;
-           AN : out STD_LOGIC;
-           DP : out STD_LOGIC);
+    Port (  clk : in STD_LOGIC;
+            rst : in std_logic;
+            seg : in STD_LOGIC;
+            CA : out STD_LOGIC;
+            CB : out STD_LOGIC;
+            CC : out STD_LOGIC;
+            CD : out STD_LOGIC;
+            CE : out STD_LOGIC;
+            CF : out STD_LOGIC;
+            CG : out STD_LOGIC;
+            AN : out STD_LOGIC;
+            DP : out STD_LOGIC
+        );
 end XSEG;
 
 architecture Behavioral of XSEG is
-begin
+
+    type state_type is (idle, handling_data, display_seg);
+
+    signal state        : state_type;
+
+    constant show_0     : std_logic_vector(6 downto 0) := "1000000";
+    constant show_1     : std_logic_vector(6 downto 0) := "1111001";
+    constant show_2     : std_logic_vector(6 downto 0) := "0100100";
+    constant show_3     : std_logic_vector(6 downto 0) := "0110000";
+    constant show_4     : std_logic_vector(6 downto 0) := "0011001";
+    constant show_5     : std_logic_vector(6 downto 0) := "0010010";
+    constant show_6     : std_logic_vector(6 downto 0) := "0000010";
+    constant show_7     : std_logic_vector(6 downto 0) := "1111000";
+    constant show_8     : std_logic_vector(6 downto 0) := "0000000";
+    constant show_9     : std_logic_vector(6 downto 0) := "0011000";
+    constant show_rst   : std_logic_vector(6 downto 0) := "0000000";
+
+    signal sig_show_seg_1 : std_logic_vector(6 downto 0);
+    signal sig_show_seg_2 : std_logic_vector(6 downto 0);
+    signal sig_show_seg_3 : std_logic_vector(6 downto 0);
+
+    begin
+
+        if clk'event and clk = '1' then
+
+            if(rst = '1') then
+
+                sig_show_seg_1 <= (others => '0');
+                sig_show_seg_2 <= (others => '0');
+                sig_show_seg_3 <= (others => '0');
+
+                
+
+            end if;
 
 
--- Prevedenou hodnotu prevede na katody segmentovky
+
+        end if;
 
 
 
--- clk_en.vhd
+++ clk_en.vhd
@@ -1,43 +1,47 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/02/2025 01:17:46 PM
--- Design Name: 
--- Module Name: clk_en - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity clk_en is
---  Port ( );
+    generic(
+        n_periods : integer := 3
+    );
+    port(
+        clk     :   in  std_logic;
+        rst     :   in  std_logic;
+        pulse   :   out std_logic
+    );
 end clk_en;
 
 architecture Behavioral of clk_en is
 
-begin
+    signal sig_count_clk : integer range 0 to n_periods - 1;
+
+    begin
+
+        proc_clk_en : process (clk) is
+        
+        begin
+
+            if clk'event and clk = '1' then
+
+                if(rst = '1') then
+                    sig_count_clk <= 0;
+
+                elsif(sig_count_clk < (n_periods - 1)) then
+                    sig_count_clk <= sig_count_clk + 1;
+
+                else
+                    sig_count_clk <= 0;
+
+                end if;
+
+            end if;
+
+        end process;
+
+    pulse <= '1' when (sig_count_clk = n_periods - 1) 
+            else '0';
+
 
 
 end Behavioral;
